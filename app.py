d8b8d9fbe6c0def5c9047473b8a35a12ac04d489
Abonty18 <labibafarah2998@gmail.com>
2024-11-08 00:09:03 +0600

    let's see :)))

--- app.py
+++ app.py
@@ -29,7 +29,5 @@ def load_user(user_id):
     from models import User
     return User.query.get(int(user_id))
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    app.run(debug=True)
