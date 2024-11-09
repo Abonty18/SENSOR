81bdaa73839fcaeeda1441604fb278fea2b98c70
Abonty <labibafarah@iut-dhaka.edu>
2024-11-09 12:53:58 +0600

    back to old one

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-# config.py
+# # config.py
+# import os
+
+# class Config:
+#     SECRET_KEY = os.urandom(24)
+#     SQLALCHEMY_DATABASE_URI = 'sqlite:///annotations.db'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
 import os
 
 class Config:
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///annotations.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
