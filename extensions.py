bc1be8ad7470a0a2fb14fb2e005aca2965285ba4
Abonty18 <labibafarah2998@gmail.com>
2025-12-06 11:42:01 -0500

    Use threading async_mode for SocketIO

--- extensions.py
+++ extensions.py
@@ -1,3 +1,4 @@
+# extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -6,7 +7,9 @@ from flask_socketio import SocketIO
 db = SQLAlchemy()
 login_manager = LoginManager()
 mail = Mail()
+
+# 👇 Important: force threading mode, not eventlet/gevent
 socketio = SocketIO(
     cors_allowed_origins="*",
-    async_mode="eventlet"
+    async_mode="threading"
 )
